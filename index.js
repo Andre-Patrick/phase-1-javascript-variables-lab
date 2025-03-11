2c89cd4fcf5784de1ceaa7e18f087f4a331197e9
Patrick <patrick.andre@student.moringaschool.com>
2025-03-11 08:22:42 +0300

    Second Try

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let companyName= 'Scuber';
-let mostProfitableNeighborhood= 'Chelsea';
 const companyName= 'Scuber';
+let mostProfitableNeighborhood= 'Chelsea';
+const CompanyName= 'Scuber';
 let companyCeo= 'Susan Smith';
+
